a7b1611bd97374bf23d1d1bff1a80a913c95208c
EnockOMONDI <djseanizellkenya@gmail.com>
2018-05-14 02:00:34 +0300

    added a database for my app and configured the db settings inside my django project

--- insta/settings.py
+++ insta/settings.py
@@ -75,13 +75,15 @@ WSGI_APPLICATION = 'insta.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'instagram',
+        'USER': 'sean',
+        'PASSWORD': 'Aggreyomondi90',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
