8f1f3ea8603e3f1b1288a88171adcf8c6548dc65
EnockOMONDI <djseanizellkenya@gmail.com>
2018-05-14 11:32:39 +0300

    added url  route for search and upload image function

--- instapp/urls.py
+++ instapp/urls.py
@@ -8,6 +8,8 @@ urlpatterns=[
 #..............
 url('^$',views.home,name='home'),
 url(r'^profile/',views.profile, name='profile'),
+url(r'^search/',views.search_results,name='search'),
+url(r'^upload_image/',views.upload,name='upload'),
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
