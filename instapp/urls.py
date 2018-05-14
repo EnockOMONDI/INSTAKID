c5c602024dfcfe34f9ab974fe80e9f811376e9de
EnockOMONDI <djseanizellkenya@gmail.com>
2018-05-14 12:19:48 +0300

    added my nav styles

--- instapp/urls.py
+++ instapp/urls.py
@@ -10,6 +10,7 @@ url('^$',views.home,name='home'),
 url(r'^profile/',views.profile, name='profile'),
 url(r'^search/',views.search_results,name='search'),
 url(r'^upload_image/',views.upload,name='upload'),
+url(r'^edit/',views.edit,name='edit'),
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
