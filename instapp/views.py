71f9151eeb7690d0fbcdf3103bfc8afdf5d83502
EnockOMONDI <djseanizellkenya@gmail.com>
2018-05-14 05:12:37 +0300

    defined my upload and edit methods

--- instapp/views.py
+++ instapp/views.py
@@ -36,3 +36,42 @@ def settings(request):
     settings = Profile.get_profile()
     return render(request,'profile/settings.html',{"settings":settings,
                                                     "title":title,})
+
+
+
+@login_required(login_url='/accounts/login/')
+def edit(request):
+    title = 'Insta-Gram'
+    current_user = request.user
+    if request.method == 'POST':
+        form = EditProfileForm(request.POST,request.FILES)
+        if form.is_valid():
+            update = form.save(commit=False)
+            update.user = current_user
+            update.save()
+            return redirect('profile')
+    else:
+        form = EditProfileForm()
+    return render(request,'profile/edit.html',{"title":title,
+                                                "form":form})
+
+@login_required(login_url="/accounts/login/")
+def upload(request):
+    title = 'Insta-Gram'
+    current_user = request.user
+    profiles = Profile.get_profile()
+    for profile in profiles:
+        if profile.user.id == current_user.id:
+            if request.method == 'POST':
+                form = UploadForm(request.POST,request.FILES)
+                if form.is_valid():
+                    upload = form.save(commit=False)
+                    upload.user = current_user
+                    upload.profile = profile
+                    upload.save()
+                    return redirect('home')
+            else:
+                form = UploadForm()
+            return render(request,'upload/new.html',{"title":title,
+                                                    "user":current_user,
+                                                    "form":form})
