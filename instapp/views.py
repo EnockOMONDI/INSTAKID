d7ed846e769460baeccf089cb142b4044985cd5e
EnockOMONDI <djseanizellkenya@gmail.com>
2018-05-14 05:06:56 +0300

    defined my profile view function

--- instapp/views.py
+++ instapp/views.py
@@ -15,3 +15,16 @@ def home(request):
                                         "comments":comments,
                                         "current_user":current_user,
                                         "images":image,})
+
+@login_required(login_url='/accounts/login/')
+def profile(request):
+    title = 'Insta-Gram'
+    current_user = request.user
+    profile = Profile.get_profile()
+    image = Image.get_images()
+    comments = Comment.get_comment()
+    return render(request,'profile/profile.html',{"title":title,
+                                                  "comments":comments,
+                                                  "image":image,
+                                                  "user":current_user,
+                                                  "profile":profile,})
