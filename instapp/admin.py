52145ee22ae121a700b0fa52cd8f3afef8d4f48c
EnockOMONDI <djseanizellkenya@gmail.com>
2018-05-14 03:19:00 +0300

    created a super user and added my models to the admin site

--- instapp/admin.py
+++ instapp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Image,Profile,Comment
 
 # Register your models here.
+
+admin.site.register(Image)
+admin.site.register(Profile)
+admin.site.register(Comment)
