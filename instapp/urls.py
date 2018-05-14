cfc47e81c6d5254d2361d19a498a4d8fce02bc74
EnockOMONDI <djseanizellkenya@gmail.com>
2018-05-14 03:51:47 +0300

    defined my base routue

--- instapp/urls.py
+++ instapp/urls.py
@@ -1,12 +1,12 @@
 from django.conf.urls import url
 from . import views
-
 from django.conf import settings
 from django.conf.urls.static import static
 #..............
 
 urlpatterns=[
 #..............
+url('^$',views.home,name='home'),
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
