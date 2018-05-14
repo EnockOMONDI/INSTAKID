3cfb0632bac9fe65716d2f87e51c1d0ec91bdcae
EnockOMONDI <djseanizellkenya@gmail.com>
2018-05-14 10:18:22 +0300

    added a url route for profiles

--- instapp/urls.py
+++ instapp/urls.py
@@ -7,6 +7,7 @@ from django.conf.urls.static import static
 urlpatterns=[
 #..............
 url('^$',views.home,name='home'),
+url(r'^profile/',views.profile, name='profile'),
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
