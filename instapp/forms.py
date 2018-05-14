b0cf2070e87da8fa3fc12ef41f64fc4da8a4a4c6
EnockOMONDI <djseanizellkenya@gmail.com>
2018-05-14 09:18:34 +0300

    added an upload form

--- instapp/forms.py
+++ instapp/forms.py
@@ -1,7 +1,14 @@
 from django import forms
 from .models import Image,Profile,Comment
 
+
 class EditProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
         exclude = ['user']
+
+
+class UploadForm(forms.ModelForm):
+    class Meta:
+        model = Image
+        exclude = ['user','likes','upload_date','profile']
