11c126480216c8b0f3aa9a19159504eac0964bd6
EnockOMONDI <djseanizellkenya@gmail.com>
2018-05-14 12:33:18 +0300

    configured all my url routes for the view functions

--- instapp/urls.py
+++ instapp/urls.py
@@ -11,6 +11,12 @@ url(r'^profile/',views.profile, name='profile'),
 url(r'^search/',views.search_results,name='search'),
 url(r'^upload_image/',views.upload,name='upload'),
 url(r'^edit/',views.edit,name='edit'),
+url(r'^settings/',views.settings,name='settings'),
+url(r'^comment/(?P<pk>\d+)',views.new_comment,name='comment'),
+url(r'^view_profile/(?P<pk>\d+)',views.view_your_profile,name='yourprofile'),
+url(r'^like/(?P<operation>.+)/(?P<pk>\d+)',views.like, name='like'),
+]
+
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
