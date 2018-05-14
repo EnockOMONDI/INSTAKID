484a8d3535dbb5d9cf7576ab3702258e5ed05f1c
EnockOMONDI <djseanizellkenya@gmail.com>
2018-05-14 04:49:45 +0300

    imported the login required decorator and used it before my home view function to limmit access to auth users then created my home view function

--- instapp/views.py
+++ instapp/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from .models import Image,Profile,Comment
 
 # Create your views here.
+@login_required(login_url='/accounts/login/')
+def home(request):
+    title = 'Insta-Gram'
+    current_user = request.user
+    profile = Profile.get_profile()
+    image = Image.get_images()
+    comments = Comment.get_comment()
+    return render(request,'index.html',{"title":title,
+                                        "profile":profile,
+                                        "comments":comments,
+                                        "current_user":current_user,
+                                        "images":image,})
