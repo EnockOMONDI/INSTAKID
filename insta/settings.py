61b29f70d2ab776ed2ead0e7ff1e8c91801cbe3a
EnockOMONDI <djseanizellkenya@gmail.com>
2018-05-21 11:06:34 +0300

    pushed to heruku

--- insta/settings.py
+++ insta/settings.py
@@ -24,9 +24,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '!23msqn8=nxnw)f-yg#6vtb@c27_3fis9u@w38*rmag073o_+0'
+SECRET_KEY=config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG',default=False , cast=bool )
+
+
 
 ALLOWED_HOSTS = ['*']
 
